e3fcdc74cf4eeaa48103739a83c35bc29bc6a6b8
Rafael <rafaelg202@gmail.com>
2018-06-10 17:44:01 -0300

    Managed, Transiant, Persist, Detached

--- src/br/com/caelum/financas/teste/TesteBuscaConta.java
+++ src/br/com/caelum/financas/teste/TesteBuscaConta.java
@@ -1,10 +1,25 @@
 package br.com.caelum.financas.teste;
 
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
 public class TesteBuscaConta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Conta conta = em.find(Conta.class, 2);
+		
+		conta.setTitular("Juca");
+		
+		em.getTransaction().commit();
+		
 	}
 
 }
