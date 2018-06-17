1c744216e06c2a733a1f876d2f82c293a2fed75e
Rafael <rafaelg202@gmail.com>
2018-06-17 11:05:21 -0300

    Modelando cardinalidades

--- src/br/com/caelum/financas/teste/TesteJPA.java
+++ src/br/com/caelum/financas/teste/TesteJPA.java
@@ -1,4 +1,4 @@
-package br.com.caelum.financas.jpa;
+package br.com.caelum.financas.teste;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
--- src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
+++ src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
@@ -1,8 +1,13 @@
 package br.com.caelum.financas.teste;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteJPARelacionamento {
@@ -10,15 +15,28 @@ public class TesteJPARelacionamento {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		EntityManager em = new JPAUtil().getEntityManager();
+		Conta conta = new Conta();
+		conta.setAgencia("0102");
+		conta.setBanco("Itau");
+		conta.setNumero("12345");
+		conta.setTitular("Rafael");
 		
+		Movimentacao mvt = new Movimentacao();
+		mvt.setData(Calendar.getInstance());
+		mvt.setDescricao("churrascaria");
+		mvt.setTipo(TipoMovimentacao.SAIDA);
+		mvt.setValor(new BigDecimal("200.0"));
+		
+		mvt.setConta(conta);
+		
+		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 		
-		Conta conta = em.find(Conta.class, 2);
-		
-		conta.setTitular("Juca");
+		em.persist(conta);
+		em.persist(mvt);
 		
 		em.getTransaction().commit();
+		em.close();
 	}
 
 }
